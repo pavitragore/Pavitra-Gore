3f157987ff45b92d4ee0e6d5477fc7c2c9eeea8f
Pavitra Gore <gorepavitra@gmail.com>
2026-04-08 22:58:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const pages = document.querySelectorAll(".page");
 function showOnScroll() {
     pages.forEach(page => {
         const offset = page.offsetTop - 300;
-
         if (window.scrollY > offset) {
             page.style.opacity = "1";
             page.style.transform = "translateY(0)";
@@ -13,3 +12,11 @@ function showOnScroll() {
 
 window.addEventListener("scroll", showOnScroll);
 window.addEventListener("load", showOnScroll);
+
+/* NAVBAR TOGGLE */
+const menu = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
